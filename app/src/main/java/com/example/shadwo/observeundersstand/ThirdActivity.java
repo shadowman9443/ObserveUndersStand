4b5105906bc820c426b102caa3db0b2ef629de00
DTL <shadowman9443@gmail.com>
2017-07-11 11:49:36 +0600

    observing from all activity

--- app/src/main/java/com/example/shadwo/observeundersstand/ThirdActivity.java
+++ app/src/main/java/com/example/shadwo/observeundersstand/ThirdActivity.java
@@ -2,12 +2,30 @@ package com.example.shadwo.observeundersstand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ThirdActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+public class ThirdActivity extends AppCompatActivity implements Observer{
+    TextView addingValue,ovserverValue;
+    double i=0;
+    MontuApp montuApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        montuApp = (MontuApp) getApplication();
+        montuApp.getObserver().addObserver(this);
+        addingValue= (TextView) findViewById(R.id.addingValue);
+        ovserverValue= (TextView) findViewById(R.id.ovserverValue);
+        double k=  montuApp.getObserver().getValue();
+        ovserverValue.setText(String.valueOf(k));
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
